d24d0dc655087675371728e93dedf2418036fdb2
abcodp-tech <abcodp@gmail.com>
2026-09-04 00:12:50 +0100

    Update verify-payment.js

--- api/verify-payment.js
+++ api/verify-payment.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
 
         return (
           session.payment_status === "paid" &&
-          session.payment_status === "paid" &&
+          
           session.amount_total === 499 &&
 session.currency === "gbp" &&
           paymentEmail === email
