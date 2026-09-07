ce650bd974be190cecc01945bebf1d10a714e3ed
abcodp-tech <abcodp@gmail.com>
2026-09-07 08:34:25 +0100

    Fix import statement syntax in value.js

--- api/value.js
+++ api/value.js
@@ -1,4 +1,4 @@
-.import { neon } from "@neondatabase/serverless";
+import { neon } from "@neondatabase/serverless";
 function getText(data) {
   return data.output_text || data.output?.flatMap(x => x.content || [])
     .filter(x => x.type === "output_text").map(x => x.text).join("\n") || "";
