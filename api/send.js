9536f3070f657d3bcb09d709692a45bb4479fba9
abcodp-tech <abcodp@gmail.com>
2026-09-08 20:02:25 +0100

    Add extraPhotoRequest to request body handling

--- api/send.js
+++ api/send.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
       high = "",
       confidence = "",
       explanation = "",
+      extraPhotoRequest = "",
       language = "en"
     } = req.body || {};
 
